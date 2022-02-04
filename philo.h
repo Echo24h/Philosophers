31e397b61ad8d7d0a78b36b477d49c8b2dad15df
Gregory Borne <gborne@c4r2p2.42nice.fr>
2022-02-04 09:10:48 +0100

    strat

---
+++ philo.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gborne <gborne@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/04 09:05:27 by gborne            #+#    #+#             */
+/*   Updated: 2022/02/04 09:06:26 by gborne           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# ifndef TRUE
+#  define TRUE 1
+# endif
+
+# ifndef FALSE
+#  define FALSE 0
+# endif
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#endif
+
